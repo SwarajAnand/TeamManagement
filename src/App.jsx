4f046e1a50ad48df050a6ab6ab5b3da815959d33
Swaraj Anand <swrjnnd@gmail.com>
2024-05-25 15:29:55 +0530

    Added files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import LeftContainer from "./components/LeftContainer"
+import RightContainer from "./components/RightContainer"
+
+function App() {
+
+  return (
+    <div className="mainDiv">
+      <LeftContainer />
+      <RightContainer />
+    </div>
+  )
+}
+
+export default App
